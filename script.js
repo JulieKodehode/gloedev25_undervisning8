ce9d5e70f49a8d9495f0bc8bca01155ee5c84f8d
JulieKodehode <julie.kodehode@gmail.com>
2025-04-30 12:07:57 +0200

    After lesson

--- script.js
+++ script.js
@@ -1,53 +1,3 @@
-// DATATYPES
-// Primative datatypes:
-// strig = tekst
-// number = tall
-// boolean = sant/usant sjekk
-
-// VARAIBLES
-// let = kan bytte verdi
-// const = kan ikke bytte verdi
-// var = utdatert
-
-let userName = "Julie";
-userName = "JulieIsobelle";
-console.log(userName);
-console.log(typeof userName);
-
-let userAge = 28;
-console.log(userAge);
-console.log(typeof userAge);
-
-let isPresent = true;
-console.log(isPresent);
-console.log(typeof isPresent);
-
-const firstName = "Julie";
-// firstName = "Julius";
-console.log(firstName);
-
-const lastName = "Isobelle";
-
-// Variable chaining med +
-let fullName = firstName + " " + lastName + "!";
-// Template litteral
-fullName = `${firstName} ${lastName}!!`;
-console.log(fullName);
-
-const num1 = 10;
-const num2 = 10;
-const sum = num1 + num2;
-console.log(sum);
-
-// NON-PRIMATIVE DATATYPES
-// Function = En blokk med kode som kjøres samtidig
-// Array = En liste av data
-// Object = key:value liste av data
-
-//
-console.log("-------------------------------");
-//
-
 function buisnessCard() {
     console.log("Hei fra inni en funksjon");
 
@@ -68,10 +18,6 @@ function buisnessCard() {
     const description = "Kurs veileder";
     console.log(`${firstName} ${lastName}, ${description}.`)
     const image = "https://images.unsplash.com/photo-1745770998338-eb50b0c89b16?q=80&w=3271&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-
-    // Kjapp introduksjon til DOMM - mer om dette på onsdag 30.04
-    // const main = document.querySelector("main");
-    // main.textContent = `${firstName} ${lastName}, ${description}.`;
 
     // Manipulerer variablene sammen
     h2Title.textContent = fullName;
